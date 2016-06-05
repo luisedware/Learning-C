349a1b49f24733efd092204c185ceb20acbc1b3a
luisedware <luisedware@qq.com>
2016-06-05 10:27:55 +0800

    学习C

--- C-Primer-Plus/5/5.13/addemup.c
+++ C-Primer-Plus/5/5.13/addemup.c
@@ -4,22 +4,12 @@
 /* 求出前20个整数的和 */
 int main(int argc, char const *argv[])
 {
-	int count, sum; /* 声明语句 */
-
-	count = 0;	/* 赋值语句 */
-	sum = 0;	/* 赋值语句 s*/
-	while(count++ < 20 ){	/* 结构化语句 */
-		sum = sum + count;
-	}
-	/* 空语句 */
-	printf("sum = %d \n",sum); /* 函数语句 */
-
-	/* 符合语句 */
-	while(years < 100){
-		wisdom = wisdom * 1.05;
-		printf("%d %d\n",years, wisdom);
-		years = years + 1;
-	}
-
-	return 0;
+    int count, sum;
+    count = 0;
+    sum = 0;
+    while (count++ < 20) {
+        sum = sum + count;
+    }
+    printf("sum = %d\n",sum);
+    return 0;
 }
