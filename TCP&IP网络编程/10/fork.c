08df8c0bb7c71e57dd76392a617a12814fdad0ae
Sebastian <sebastiankennedy@foxmail.com>
2019-07-02 12:51:17 +0800

    TCP&IP网络编程 - 第十章

--- TCP&IP网络编程/10/fork.c
+++ TCP&IP网络编程/10/fork.c
@@ -7,12 +7,16 @@ int main(int argc, char *argv[]) {
   int lval = 20;
   gval++, lval += 5;
 
+  // fork 函数创建进程副本
   pid = fork();
+
+  // 父进程和子进程都将执行 fork 函数调用后的语句
   if (pid == 0)
     gval += 2, lval += 2;
   else
     gval -= 2, lval -= 2;
 
+  // PID 为 0 为子进程
   if (pid == 0)
     printf("Child Proc: [%d, %d] \n", gval, lval);
   else
--- TCP&IP网络编程/10/zombie.c
+++ TCP&IP网络编程/10/zombie.c
@@ -5,17 +5,24 @@ int main(int argc, char *argv[]) {
   pid_t pid = fork();
 
   if (pid == 0) {
+    // 子进程提前执行完毕
     puts("Hi, I am a child process");
   } else {
+    // 父进程暂定 30 秒
     printf("Child Process ID: %d \n", pid);
     sleep(30);
   }
 
   if (pid == 0) {
+    // 没有向父进程传递子进程的 exit 参数或 return
+    // 语句的返回值，子进程成为僵尸进程
     puts("End child process");
   } else {
     puts("End parent process");
   }
+
+  // 执行 ps aux | grep pid 结果
+  // sebastian  4785   0.0  0.0  0  0 s002  ZN   11:19AM   0:00.00 (zombie)
 
   return 0;
 }
